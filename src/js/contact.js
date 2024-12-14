0da4f3fa81144a16906cbb91ede75fd7dcc47c2a
Madison Hanberry <mhanberry1@gmail.com>
2024-12-14 02:19:16 -0800

    add join page

--- src/js/contact.js
+++
@@ -1,6 +0,0 @@
-import { $$ } from './common.js'
-
-console.log('here')
-$$('.faq li').forEach(e =>
-	e.onclick = ({ target }) => target.closest('li').classList.toggle('expand')
-)
---
+++ src/js/include/faq.js
@@ -0,0 +1,5 @@
+import { $$ } from './common.js'
+
+$$('.faq li').forEach(e =>
+	e.onclick = ({ target }) => target.closest('li').classList.toggle('expand')
+)
